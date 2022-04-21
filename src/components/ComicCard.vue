77d6b69c05dcc642ad521f45d1b38c353d3858c1
yohannmarchand <marchandyohannnnn@gmail.com>
2022-04-21 11:13:27 +0200

    create comics card

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <RouterLink :to="`/comic/${comic.id}`">
+      <div>
+        <img
+          class="img-fluid"
+          :src="thumbnail"
+          :alt="comic.title"
+        >
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comic: Object
+  },
+
+  computed: {
+    thumbnail() {
+      return this.comic.images[0] ? `${this.comic.images[0].path}.${this.comic.images[0].extension}` : `${this.comic.thumbnail.path}.${this.comic.thumbnail.extension}`
+    }
+  }
+}
+</script>
