75316e4ff6b68b15f3d47726775f8262374123f7
yohannmarchand <marchandyohannnnn@gmail.com>
2022-04-21 15:07:27 +0200

    refacto vue store on home view

--- src/store/comics.js
+++ src/store/comics.js
@@ -5,51 +5,16 @@ export default {
   namespaced: true,
 
   state: () => ({
-    comics: [],
     detail: {},
-    pagination: 0,
-    limit: 20,
   }),
 
-  getters: {
-    GET_COMICS_BY_SEARCH: (state) => (value) => {
-      return state.comics.filter(comic => comic.title.toLowerCase().includes(value.toLowerCase()))
-    }
-  },
-
   mutations: {
-    ADD_COMICS(state, comics) {
-      comics.forEach(comic => {
-        if (!state.comics.find(c => c.id === comic.id)) {
-          state.comics.push(comic)
-        }
-      })
-    },
-
-    SET_PAGINATION(state, pagination) {
-      state.pagination = pagination
-    },
-
     GET_DETAIL(state, detail) {
       state.detail = detail;
     }
   },
 
   actions: {
-    FETCH_COMICS() {
-      $api.get('/comics').then(({ data }) => {
-        store.commit('comics/SET_PAGINATION', (data.data.count + data.data.offset) / data.data.limit)
-        store.commit('comics/ADD_COMICS', data.data.results)
-      })
-    },
-
-    FETCH_NEXT_PAGE({ state }) {
-      $api.get(`/comics?offset=${state.pagination * state.limit}`).then(({ data }) => {
-        store.commit('comics/SET_PAGINATION', (data.data.count + data.data.offset) / data.data.limit)
-        store.commit('comics/ADD_COMICS', data.data.results)
-      })
-    },
-
     FETCH_COMIC_DETAIL({ state }, payload) {
       $api.get(`/comics/${payload}`).then(({ data }) => {
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-      class="home"
-      v-infinite-scroll="loadMore"
-      infinite-scroll-distance="200"
+    class="home"
+    v-infinite-scroll="loadMore"
+    infinite-scroll-distance="200"
   >
     <SearchBar />
-    <Catalog :comics="search ? getComicsBySearch(search) : comics" />
+    <Catalog :comics="search ? getComicsBySearch : comics" />
     <div v-if="isFetchingData" class="d-flex justify-content-center my-5">
       <div
-          v-for="i in 3"
-          :key="i"
+        v-for="i in 3"
+        :key="i"
       >
         <div class="spinner-grow text-danger mx-2" role="status">
           <span class="visually-hidden">Loading...</span>
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import {mapGetters, mapState} from "vuex";
+import { $api } from "../plugin/axios";
 
 import Catalog from '@/components/Catalog.vue'
 import SearchBar from '@/components/SearchBar.vue'
@@ -36,17 +36,16 @@ export default {
   data() {
     return {
       isFetchingData: false,
+      comics: [],
+      pagination: 0,
+      limit: 20
     }
   },
 
   computed: {
-    ...mapState('comics', {
-      comics: 'comics'
-    }),
-
-    ...mapGetters('comics',{
-      getComicsBySearch: 'GET_COMICS_BY_SEARCH'
-    }),
+    getComicsBySearch() {
+      return this.comics.filter(comic => comic.title.toLowerCase().includes(this.search.toLowerCase()))
+    },
 
     search() {
       return this.$route.query.q
@@ -55,13 +54,17 @@ export default {
 
   methods: {
     loadMore() {
-      if (!this.isFetchingData || !this.search) {
+      if (!this.isFetchingData && !this.search) {
         this.isFetchingData = true
-        this.$store.dispatch('comics/FETCH_NEXT_PAGE')
 
-        setTimeout(() => {
-          this.isFetchingData = false
-        }, 2000)
+        $api.get(`/comics?offset=${this.pagination * this.limit}`).then(({ data }) => {
+          this.pagination ++
+          data.data.results.forEach(comic => {
+            if (!this.comics.find(c => c.id === comic.id)) {
+              this.comics.push(comic)
+            }
+          })
+        }).then(() => this.isFetchingData = false)
       }
     },
   }
