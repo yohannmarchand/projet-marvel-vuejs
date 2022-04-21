c8fca21d188d973415e85ad2067a5c26ade2665c
yohannmarchand <marchandyohannnnn@gmail.com>
2022-04-21 16:25:06 +0200

    fix: thumbnail provoc console error

--- src/views/ComicsDetail.vue
+++ src/views/ComicsDetail.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="about d-flex flex-column align-items-center">
-    <h1 class="text-center">{{comic.title}}</h1>
+    <h1 class="text-center">{{ comic.title }}</h1>
     <img
-      class="img-thumbnail img-fluid w-25"
-      :src="comicThumbnail"
-      :alt="comic.title"
+        class="img-thumbnail img-fluid w-25"
+        :src="comicThumbnail"
+        :alt="comic.title"
     >
-    <small>Couverture de <b>{{comic.title}}</b></small>
+    <small>Couverture de <b>{{ comic.title }}</b></small>
 
     <h2 class="mt-5 mb-3">Caractères :</h2>
     <div class="container">
@@ -28,19 +28,19 @@
 </template>
 
 <script>
-import { $api } from "../plugin/axios";
+import {$api} from "../plugin/axios";
 
 export default {
   name: 'ComicsDetail',
 
-  computed: { 
+  computed: {
     comicThumbnail() {
-      return this.comic ? this.comic.images[0] ? `${this.comic.images[0].path}.${this.comic.images[0].extension}` : `${this.comic.thumbnail.path}.${this.comic.thumbnail.extension}` : '';
+      return this.comic.id ? this.comic.images.length ? `${this.comic.images[0].path}.${this.comic.images[0].extension}` : `${this.comic.thumbnail.path}.${this.comic.thumbnail.extension}` : '';
     },
   },
 
   methods: {
-	characterThumbnail(character) {
+    characterThumbnail(character) {
       return character ? `${character.thumbnail.path}.${character.thumbnail.extension}` : '';
     },
   },
@@ -53,14 +53,14 @@ export default {
   },
 
   created() {
-    $api.get(`/comics/${this.$route.params.id}`).then(({ data }) => {
+    $api.get(`/comics/${this.$route.params.id}`).then(({data}) => {
       this.comic = data.data.results[0];
-    }).then(() => {
-      	$api.get(`/comics/${this.$route.params.id}/characters`)
-      	.then(({ data }) => {  
-			this.characters = data.data.results;
-		})
-	})
+    })
+    .then(() => {
+      $api.get(`/comics/${this.$route.params.id}/characters`).then(({data}) => {
+        this.characters = data.data.results;
+      })
+    })
   }
 }
 </script>
