79559e7a2e22e160995591402959d4d7a129015b
UserWithoutAName <pro.leguilloux.lucas@gmail.com>
2022-04-21 14:10:49 +0200

    début de page de détail

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-    <h1>{{this.$route.params.id}}</h1>
+    <h1>{{comic.title}}</h1>
+    <img
+      class="img-fluid img-thumbnail"
+      :src="thumbnail"
+      :alt="comic.title"
+    >
   </div>
 </template>
 
@@ -10,9 +15,15 @@ import { mapState } from 'vuex'
 export default {
   name: 'Detail',
 
-  computed: mapState("comics", {
-    comic: "detail"
-  }),
+  computed: { 
+    ...mapState("comics", {
+      comic: "detail"
+    }),
+
+    thumbnail() {
+      return this.comic.images[0] ? `${this.comic.images[0].path}.${this.comic.images[0].extension}` : `${this.comic.thumbnail.path}.${this.comic.thumbnail.extension}`
+    }
+  },
 
   created() {
     this.$store.dispatch('comics/FETCH_COMIC_DETAIL', this.$route.params.id)
