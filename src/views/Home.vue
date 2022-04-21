902457921afb65c5da94d82e4191690a147c20e0
yohannmarchand <marchandyohannnnn@gmail.com>
2022-04-21 10:21:17 +0200

    undo test code in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,24 +8,11 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import {mapState} from "vuex";
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
   },
-
-  created() {
-    this.$store.dispatch('comics/FETCH_COMICS').then(() => {
-      this.$store.dispatch('comics/FETCH_NEXT_PAGE')
-    })
-  },
-
-  computed: {
-    ...mapState('comics', {
-      comics: 'comics'
-    }),
-  }
 }
 </script>
