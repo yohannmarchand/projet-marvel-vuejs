2ab8550de1638f27194d6da89c488d5cdfac1fd9
yohannmarchand <marchandyohannnnn@gmail.com>
2022-04-21 11:54:43 +0200

    change path for detail view

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: '/detail/:id?',
+    path: '/comic/:id',
     name: 'Detail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
